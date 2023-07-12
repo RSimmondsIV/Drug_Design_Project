c19f1aef0d9fb0848dedabb1ff9a378cd307805a
RSimmondsIV <RSimmondsIV2006@gmail.com>
2023-07-12 10:31:14 +0200

    Set up Folders and removed unneeded info.

--- RDKit_Algorithms/reading_single_mol.py
+++
@@ -1,8 +0,0 @@
-from rdkit import Chem
-from rdkit.Chem import Draw
-
-# Makes a Molecule from SMILES notation
-m = Chem.MolFromSmiles('Cc1ccccc1')
-print(type(m))
-img = Draw.MolToImage(m)
-img.show()
