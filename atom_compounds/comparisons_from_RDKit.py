cbdbad6685eb9489aeb04a6be055172a651ec8f8
RSimmondsIV <RSimmondsIV2006@gmail.com>
2023-07-14 10:45:53 +0200

    Small Changes

--- atom_compounds/comparisons_from_RDKit.py
+++ atom_compounds/comparisons_from_RDKit.py
@@ -2,6 +2,7 @@ from atom_pair_fingerprint.atom_pair_fingerprints import smiles_to_fingerprint a
 from MACCS_keys_fingerprint.MACCS_keys_fingerprints import smiles_to_fingerprint as mk
 from morgan_fingerprint.morgan_fingerprints import smiles_to_fingerprint as mf
 from topological_torsion_fingerprint.topological_torsion_fingerprints import smiles_to_fingerprint as tt
+from one_hot_encoding.one_hot_encoding import one_hot_encode as oh
 
 SMILES = "CC(Sc1nc(N)cc(N)n1)c1nc(C(C)(C)C)no1"
 
@@ -9,5 +10,5 @@ atom_pair = ap(SMILES)
 MACCS = mk(SMILES)
 morgan = mf(SMILES)
 topological = tt(SMILES)
+ONEHOT = oh(SMILES)
 
-all = [atom_pair, MACCS, morgan, topological]
